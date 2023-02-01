e5930c3f799ae55bcc84954bbcf0e0ecb0cf9f6b
hkijmk <hkijmk@proton.me>
2023-02-01 14:17:44 -0600

    Updated the frequently asked questions.

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 
-import { FAQ } from '../../constants';
-
 @Component({
     selector: 'faq',
     templateUrl: './faq.component.html',
     styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent {
-    FAQ = FAQ;
-
     constructor() {
     }
 }
--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,4 +1,3 @@
 export * from './block-chain-public-keys.const';
-export * from './faq.const';
 export * from './icon-svgs.const';
 export * from './keyboard-keys.const';
